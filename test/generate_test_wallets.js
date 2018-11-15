207bea67fb7c45c650c1d0daaa83ea0ea07ee3dc
Jared Tobin <jared@jtobin.io>
2018-11-15 13:29:42 +1300

    Update tests.
    Also adds a simple script for regenerating wallet snapshots -- i.e.
    contents of tests/assets -- whenever they need updating.  Since they
    contain version metadata, they need to be updated somewhat frequently.

---
+++ test/generate_test_wallets.js
@@ -0,0 +1,49 @@
+const fs = require('fs')
+const kg = require('../src')
+
+const write = (wal, num) => {
+  fs.writeFile(
+    `./assets/wallet${num}.json`,
+    JSON.stringify(wal),
+    _ => console.log('written')
+  )
+}
+
+const main = async () => {
+  let config = {
+    ticket: '~doznec-marbud',
+    ship: 1
+  }
+  let wallet = await kg.generateWallet(config)
+
+  write(wallet, 0)
+
+  config = {
+    ticket: '~marbud-tidsev-litsut-hidfep',
+    ship: 65012,
+    boot: true
+  }
+  wallet = await kg.generateWallet(config)
+
+  write(wallet, 1)
+
+  config = {
+    ticket: '~wacfus-dabpex-danted-mosfep-pasrud-lavmer-nodtex-taslus-pactyp-milpub-pildeg-fornev-ralmed-dinfeb-fopbyr-sanbet-sovmyl-dozsut-mogsyx-mapwyc-sorrup-ricnec-marnys-lignex',
+    passphrase: 'froot loops',
+    ship: 222,
+    revision: 6
+  }
+  wallet = await kg.generateWallet(config)
+
+  write(wallet, 2)
+
+  config = {
+    ticket: '~doznec-marbud',
+    ship: 0
+  }
+  wallet = await kg.generateWallet(config)
+
+  write(wallet, 3)
+}
+
+main()
